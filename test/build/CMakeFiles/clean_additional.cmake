4f03b297b71f49de49b4e2e185cdbf83539ac88d
xsy <3779197165@qq.com>
2026-05-10 16:32:31 +0800

    finish stage2

--- test/build/CMakeFiles/clean_additional.cmake
+++ test/build/CMakeFiles/clean_additional.cmake
@@ -1,10 +1,13 @@
 # Additional clean files
 cmake_minimum_required(VERSION 3.16)
 
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
   file(REMOVE_RECURSE
-  "CMakeFiles\\GenealogySystem_autogen.dir\\AutogenUsed.txt"
-  "CMakeFiles\\GenealogySystem_autogen.dir\\ParseCache.txt"
+  "CMakeFiles/GenealogySystem_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/GenealogySystem_autogen.dir/ParseCache.txt"
+  "CMakeFiles/Stage2Smoke_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Stage2Smoke_autogen.dir/ParseCache.txt"
   "GenealogySystem_autogen"
+  "Stage2Smoke_autogen"
   )
 endif()
